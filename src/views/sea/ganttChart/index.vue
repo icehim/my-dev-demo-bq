c4b9243445879c8b56b07c03e12089538048fea0
之铭 <zhiming.hao@qq.com>
2025-10-27 16:05:23 +0800

    feat: 更换甘特图组件

--- src/views/sea/ganttChart/index.vue
+++ src/views/sea/ganttChart/index.vue
@@ -1,166 +1,259 @@
 <script setup lang="ts">
-import 'dhtmlx-gantt/codebase/dhtmlxgantt.css';
 import { onMounted, ref } from 'vue';
-import { gantt } from 'dhtmlx-gantt';
-import dayjs from 'dayjs';
-const contRef = ref(null);
+import { Gantt, GanttConstructorOptions } from '@visactor/vtable-gantt';
 
-const tasks = {
-  data: [
-    // 船只 #1：上海 → 广州
-    {
-      id: 1,
-      text: '船只 #1',
-      start_date: '2025-04-01',
-      duration: 10,
-      order: 1,
-      progress: 0.5,
-      open: true,
-      echelon: '梯队 1',
-      name: '船只 #1 - 上海到广州',
-      start_port: '上海',
-      end_port: '广州'
-    },
-    {
-      id: 2,
-      text: '起航 - 船只 #1',
-      start_date: '2025-04-01',
-      duration: 2,
-      order: 2,
-      progress: 1,
-      parent: 1,
-      echelon: '梯队 1',
-      name: '船只 #1',
-      start_port: '上海',
-      end_port: '广州'
-    },
-    {
-      id: 3,
-      text: '航行 - 船只 #1',
-      start_date: '2025-04-03',
-      duration: 6,
-      order: 3,
-      progress: 0.3,
-      parent: 1,
-      echelon: '梯队 1',
-      name: '船只 #1',
-      start_port: '上海',
-      end_port: '广州'
-    },
-    {
-      id: 4,
-      text: '到达 - 船只 #1',
-      start_date: '2025-04-09',
-      duration: 2,
-      order: 4,
-      progress: 0,
-      parent: 1,
-      echelon: '梯队 1',
-      name: '船只 #1',
-      start_port: '上海',
-      end_port: '广州'
-    },
+const contRef = ref<HTMLElement>(null);
+const records = [
+  {
+    id: 1,
+    order: 1,
+    title: '滚装船 1',
+    start: '2024-07-24',
+    end: '2024-07-26',
+    progress: 31,
+    priority: 'P0'
+  },
+  {
+    id: 2,
+    order: 1,
+    title: '滚装船 2',
+    start: '07/24/2024',
+    end: '08/04/2024',
+    progress: 60,
+    priority: 'P0'
+  },
+  {
+    id: 3,
+    order: 1,
+    title: '滚装船 3',
+    start: '2024-08-04',
+    end: '2024-08-04',
+    progress: 100,
+    priority: 'P1'
+  },
+  {
+    id: 4,
+    order: 2,
+    title: '滚装船 4',
+    start: '2024-07-26',
+    end: '2024-07-28',
+    progress: 31,
+    priority: 'P0'
+  },
+  {
+    id: 5,
+    order: 2,
+    title: '滚装船 5',
+    start: '2024-07-26',
+    end: '2024-07-28',
+    progress: 60,
+    priority: 'P0'
+  },
+  {
+    id: 6,
+    order: 3,
+    title: '滚装船 6',
+    start: '2024-07-29',
+    end: '2024-08-11',
+    progress: 100,
+    priority: 'P1'
+  }
+];
+const columns = [
+  {
+    field: 'order',
+    title: '梯队',
+    width: 'auto',
+    editor: 'input'
+  },
+  {
+    field: 'title',
+    title: '船名',
+    width: 'auto',
+    editor: 'date-input'
+  }
+];
 
-    // 船只 #2：天津 → 深圳
-    {
-      id: 5,
-      text: '船只 #2',
-      start_date: '2025-04-01',
-      duration: 12,
-      order: 5,
-      progress: 0.6,
-      open: true,
-      echelon: '梯队 2',
-      name: '船只 #2 - 天津到深圳',
-      start_port: '天津',
-      end_port: '深圳'
-    },
-    {
-      id: 6,
-      text: '起航 - 船只 #2',
-      start_date: '2025-04-01',
-      duration: 3,
-      order: 6,
-      progress: 1,
-      parent: 5,
-      echelon: '梯队 2',
-      name: '船只 #2',
-      start_port: '天津',
-      end_port: '深圳'
-    },
-    {
-      id: 7,
-      text: '航行 - 船只 #2',
-      start_date: '2025-04-04',
-      duration: 7,
-      order: 7,
-      progress: 0.5,
-      parent: 5,
-      echelon: '梯队 2',
-      name: '船只 #2',
-      start_port: '天津',
-      end_port: '深圳'
-    },
-    {
-      id: 8,
-      text: '到达 - 船只 #2',
-      start_date: '2025-04-11',
-      duration: 2,
-      order: 8,
-      progress: 0,
-      parent: 5,
-      echelon: '梯队 2',
-      name: '船只 #2',
-      start_port: '天津',
-      end_port: '深圳'
+const options = {
+  overscrollBehavior: 'none',
+  records,
+  taskListTable: {
+    columns,
+    tableWidth: 250,
+    minTableWidth: 100,
+    maxTableWidth: 600,
+    theme: {
+      headerStyle: {
+        borderColor: '#e1e4e8',
+        borderLineWidth: 1,
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: 'red',
+        bgColor: '#EEF1F5'
+      },
+      bodyStyle: {
+        borderColor: '#e1e4e8',
+        borderLineWidth: [1, 0, 1, 0],
+        fontSize: 16,
+        color: '#4D4D4D',
+        bgColor: '#FFF'
+      }
     }
-  ]
+    //rightFrozenColCount: 1
+  },
+  frame: {
+    outerFrameStyle: {
+      borderLineWidth: 2,
+      borderColor: '#e1e4e8',
+      cornerRadius: 8
+    },
+    verticalSplitLine: {
+      lineColor: '#e1e4e8',
+      lineWidth: 3
+    },
+    horizontalSplitLine: {
+      lineColor: '#e1e4e8',
+      lineWidth: 3
+    },
+    verticalSplitLineMoveable: true,
+    verticalSplitLineHighlight: {
+      lineColor: 'green',
+      lineWidth: 3
+    }
+  },
+  grid: {
+    verticalLine: {
+      lineWidth: 1,
+      lineColor: '#e1e4e8'
+    },
+    horizontalLine: {
+      lineWidth: 1,
+      lineColor: '#e1e4e8'
+    }
+  },
+  headerRowHeight: 40,
+  rowHeight: 40,
+  taskBar: {
+    startDateField: 'start',
+    endDateField: 'end',
+    progressField: 'progress',
+    resizable: true,
+    moveable: true,
+    hoverBarStyle: {
+      barOverlayColor: 'rgba(99, 144, 0, 0.4)'
+    },
+    labelText: '{title}  complete {progress}%',
+    labelTextStyle: {
+      fontFamily: 'Arial',
+      fontSize: 16,
+      textAlign: 'left',
+      textOverflow: 'ellipsis'
+    },
+    barStyle: {
+      width: 20,
+      /** 任务条的颜色 */
+      barColor: '#ee8800',
+      /** 已完成部分任务条的颜色 */
+      completedBarColor: '#91e8e0',
+      /** 任务条的圆角 */
+      cornerRadius: 8,
+      /** 任务条的边框 */
+      borderLineWidth: 1,
+      /** 边框颜色 */
+      borderColor: 'black'
+    }
+  },
+  timelineHeader: {
+    colWidth: 100,
+    backgroundColor: '#EEF1F5',
+    horizontalLine: {
+      lineWidth: 1,
+      lineColor: '#e1e4e8'
+    },
+    verticalLine: {
+      lineWidth: 1,
+      lineColor: '#e1e4e8'
+    },
+    scales: [
+      {
+        unit: 'week',
+        step: 1,
+        startOfWeek: 'sunday',
+        format(date) {
+          return `Week ${date.dateIndex}`;
+        },
+        style: {
+          fontSize: 20,
+          fontWeight: 'bold',
+          color: 'white',
+          strokeColor: 'black',
+          textAlign: 'right',
+          textBaseline: 'bottom',
+          backgroundColor: '#EEF1F5',
+          textStick: true
+          // padding: [0, 30, 0, 20]
+        }
+      },
+      {
+        unit: 'day',
+        step: 1,
+        format(date) {
+          return date.dateIndex.toString();
+        },
+        style: {
+          fontSize: 20,
+          fontWeight: 'bold',
+          color: 'white',
+          strokeColor: 'black',
+          textAlign: 'right',
+          textBaseline: 'bottom',
+          backgroundColor: '#EEF1F5'
+        }
+      }
+    ]
+  },
+  markLine: [
+    {
+      date: '2024/8/02',
+      scrollToMarkLine: true,
+      position: 'left',
+      style: {
+        lineColor: 'red',
+        lineWidth: 1
+      }
+    }
+  ],
+  rowSeriesNumber: {
+    title: '序号',
+    dragOrder: true,
+    headerStyle: {
+      bgColor: '#EEF1F5',
+      borderColor: '#e1e4e8'
+    },
+    style: {
+      borderColor: '#e1e4e8'
+    }
+  },
+  scrollStyle: {
+    scrollRailColor: 'RGBA(246,246,246,0.5)',
+    visible: 'scrolling',
+    width: 6,
+    scrollSliderCornerRadius: 2,
+    scrollSliderColor: '#5cb85c'
+  }
 };
-
 onMounted(() => {
-  // 解析 yyyy-mm-dd 格式
-  gantt.config.date_format = '%Y-%m-%d';
-  // 顶部时间轴显示 yyyy/mm/dd
-  gantt.config.date_scale = '%Y-%m-%d';
-  gantt.config.min_column_width = 90;
-  //中文
-  gantt.i18n.setLocale('cn');
-  //只读模式
-  gantt.config.readonly = true;
-  // 开启marker插件
-  // gantt.plugins({ marker: true, tooltip: true });
-  // 自定义列头：只显示 序号 和 梯队
-  gantt.config.columns = [
-    {
-      name: 'order',
-      label: '序号',
-      width: 80,
-      align: 'center',
-      template: task => task.order || ''
-    },
-    {
-      name: 'echelon',
-      label: '梯队',
-      width: 80,
-      align: 'center',
-      template: task => task.echelon
-    },
-    {
-      name: 'name',
-      label: '船名',
-      tree: true,
-      width: 170,
-      template: task => task.name
-    }
-  ];
-  gantt.init(contRef.value);
-  gantt.parse(tasks);
+  const ganttInstance = new Gantt(
+    contRef.value,
+    options as GanttConstructorOptions
+  );
+  window['ganttInstance'] = ganttInstance;
 });
 </script>
 
 <template>
   <div>
-    <div ref="contRef" class="h-full w-full" />
+    <div ref="contRef" class="h-full w-full relative" />
   </div>
 </template>
 
