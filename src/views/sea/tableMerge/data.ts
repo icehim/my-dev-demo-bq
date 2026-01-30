ab13056f5537ae13bb7df4625f36b207836623ac
之铭 <zhiming.hao@qq.com>
2026-01-30 11:35:02 +0800

    fix: vxe表格合并

--- src/views/sea/tableMerge/data.ts
+++ src/views/sea/tableMerge/data.ts
@@ -3,90 +3,128 @@ export const tableData = [
     id: '1',
     tdmc: '第1梯队',
     name: '1',
-    xqId: 'A'
+    xqId: 'A',
+    cbmc: 'ship1',
+    zzgkmc: 'load1'
   },
   {
     tdmc: '第1梯队',
     name: '1',
     id: '2',
-    xqId: 'A'
+    xqId: 'A',
+    cbmc: 'ship1',
+    zzgkmc: 'load1'
   },
   {
     tdmc: '第1梯队',
     name: '1',
     id: '3',
-    xqId: 'A'
+    xqId: 'A',
+    cbmc: 'ship2',
+    zzgkmc: 'load2'
+  },
+  {
+    tdmc: '第1梯队',
+    name: '1',
+    id: '16',
+    xqId: 'A',
+    cbmc: 'ship2',
+    zzgkmc: 'load2'
   },
   {
     tdmc: '第2梯队',
     name: '2',
     id: '4',
-    xqId: 'A'
+    xqId: 'A',
+    cbmc: 'ship2',
+    zzgkmc: 'load2'
   },
   {
     tdmc: '第2梯队',
     name: '2',
     id: '5',
-    xqId: 'A'
+    xqId: 'A',
+    cbmc: 'ship2',
+    zzgkmc: 'load2'
   },
   {
     tdmc: '第1梯队',
     name: '1',
     id: '6',
-    xqId: 'B'
+    xqId: 'B',
+    cbmc: 'ship3',
+    zzgkmc: 'load3'
   },
   {
     tdmc: '第1梯队',
     name: '1',
     id: '7',
-    xqId: 'B'
+    xqId: 'B',
+    cbmc: 'ship3',
+    zzgkmc: 'load3'
   },
   {
     tdmc: '第2梯队',
     name: '2',
     id: '8',
-    xqId: 'B'
+    xqId: 'B',
+    cbmc: 'ship4',
+    zzgkmc: 'load4'
   },
   {
     tdmc: '第2梯队',
     name: '2',
     id: '9',
-    xqId: 'B'
+    xqId: 'B',
+    cbmc: 'ship4',
+    zzgkmc: 'load4'
   },
   {
     tdmc: '第2梯队',
     name: '2',
     id: '10',
-    xqId: 'B'
+    xqId: 'B',
+    cbmc: 'ship4',
+    zzgkmc: 'load4'
   },
   {
     tdmc: '第1梯队',
     name: '1',
     id: '11',
-    xqId: 'C'
+    xqId: 'C',
+    cbmc: 'ship5',
+    zzgkmc: 'load5'
   },
   {
     tdmc: '第2梯队',
     name: '2',
     id: '12',
-    xqId: 'C'
+    xqId: 'C',
+    cbmc: 'ship6',
+    zzgkmc: 'load6'
   },
   {
     tdmc: '第3梯队',
     name: '3',
     id: '13',
-    xqId: 'C'
+    xqId: 'C',
+    cbmc: 'ship7',
+    zzgkmc: 'load7'
   },
   {
     tdmc: '第4梯队',
     name: '4',
     id: '14',
-    xqId: 'C'
+    xqId: 'C',
+    cbmc: 'ship8',
+    zzgkmc: 'load8'
   },
   {
     tdmc: '第4梯队',
     name: '4',
     id: '15',
-    xqId: 'C'
+    xqId: 'C',
+    cbmc: 'ship8',
+    zzgkmc: 'load8'
   }
 ];
